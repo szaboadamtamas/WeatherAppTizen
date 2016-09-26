be9de89d57084b43c532a298fdf2eaa77b652dce
Ádám Szabó <szabo.adam.tamas@gmail.com>
2016-09-26 21:42:16 +0200

    HTTPS proxy added

--- js/model/city.js
+++ js/model/city.js
@@ -5,7 +5,7 @@ function City(name, data) {
 City.prototype.loadData = function () {
   var self = this;
   return $.Deferred(function (defer) {
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + self.name + '&APPID=092772ce1a4ecc1ec94acba0e815728f')
+    $.getJSON('ttps://ups.autsoft.hu/owm/weather?q=' + self.name + '&APPID=092772ce1a4ecc1ec94acba0e815728f')
       .done(function (result) {
         self.data = result;
         defer.resolve();
